d2b2ede7add4403287a23cdc1a2eacb2db157cd8
bullishgopher <49041460+bullishgopher@users.noreply.github.com>
2023-09-27 06:17:46 -0700

    add time lock

---
+++ src/crypto/ibe.ts
@@ -0,0 +1,214 @@
+import * as bls from "@noble/bls12-381";
+import { PointG1, PointG2, utils } from "@noble/bls12-381";
+import { sha256 } from "@noble/hashes/sha256";
+import { bytesToNumberBE, fp12ToBytes, xor } from "./utils";
+import { Buffer } from "buffer";
+
+export interface Ciphertext {
+  U: PointG1;
+  V: Uint8Array;
+  W: Uint8Array;
+}
+
+export interface CiphertextOnG2 {
+  U: PointG2;
+  V: Uint8Array;
+  W: Uint8Array;
+}
+
+export async function encryptOnG1(
+  master: PointG1,
+  ID: Uint8Array,
+  msg: Uint8Array
+): Promise<Ciphertext> {
+  if (msg.length >> 8 > 1) {
+    throw new Error(
+      "cannot encrypt messages larger than our hash output: 256 bits."
+    );
+  }
+
+  // 1. Compute Gid = e(master,Q_id)
+  const Qid = await bls.PointG2.hashToCurve(ID);
+  const Gid = bls.pairing(master, Qid);
+
+  // 2. Derive random sigma
+  const sigma = utils.randomBytes(msg.length);
+
+  // 3. Derive r from sigma and msg and get a field element
+  const r = h3(sigma, msg);
+  const U = bls.PointG1.BASE.multiply(r);
+
+  // 5. Compute V = sigma XOR H2(rGid)
+  const rGid = Gid.pow(r);
+  const hrGid = await gtToHash(rGid, msg.length);
+
+  const V = xor(sigma, hrGid);
+
+  // 6. Compute M XOR H(sigma)
+  const hsigma = h4(sigma, msg.length);
+
+  const W = xor(msg, hsigma);
+
+  return {
+    U: U,
+    V: V,
+    W: W,
+  };
+}
+
+export async function encryptOnG2(
+  master: PointG2,
+  ID: Uint8Array,
+  msg: Uint8Array
+): Promise<CiphertextOnG2> {
+  if (msg.length >> 8 > 1) {
+    throw new Error(
+      "cannot encrypt messages larger than our hash output: 256 bits."
+    );
+  }
+
+  // 1. Compute Gid = e(Q_id, master)
+  const Qid = await bls.PointG1.hashToCurve(ID);
+  const Gid = bls.pairing(Qid, master);
+
+  // 2. Derive random sigma
+  const sigma = utils.randomBytes(msg.length);
+
+  // 3. Derive r from sigma and msg and get a field element
+  const r = h3(sigma, msg);
+  const U = bls.PointG2.BASE.multiply(r);
+
+  // 5. Compute V = sigma XOR H2(rGid)
+  const rGid = Gid.pow(r);
+  const hrGid = await gtToHash(rGid, msg.length);
+
+  const V = xor(sigma, hrGid);
+
+  // 6. Compute M XOR H(sigma)
+  const hsigma = h4(sigma, msg.length);
+
+  const W = xor(msg, hsigma);
+
+  return {
+    U: U,
+    V: V,
+    W: W,
+  };
+}
+
+export async function decryptOnG1(
+  p: PointG2,
+  c: Ciphertext
+): Promise<Uint8Array> {
+  // 1. Compute sigma = V XOR H2(e(rP,private))
+  const gidt = bls.pairing(c.U, p);
+  const hgidt = gtToHash(gidt, c.W.length);
+
+  if (hgidt.length != c.V.length) {
+    throw new Error("XorSigma is of invalid length");
+  }
+  const sigma = xor(hgidt, c.V);
+
+  // 2. Compute M = W XOR H4(sigma)
+  const hsigma = h4(sigma, c.W.length);
+
+  const msg = xor(hsigma, c.W);
+
+  // 	3. Check U = rP
+  const r = h3(sigma, msg);
+  const rP = bls.PointG1.BASE.multiply(r);
+
+  if (!rP.equals(c.U)) {
+    throw new Error("invalid proof: rP check failed");
+  }
+
+  return msg;
+}
+
+export async function decryptOnG2(
+  p: PointG1,
+  c: CiphertextOnG2
+): Promise<Uint8Array> {
+  // 1. Compute sigma = V XOR H2(e(private, rP))
+  const gidt = bls.pairing(p, c.U);
+  const hgidt = gtToHash(gidt, c.W.length);
+
+  if (hgidt.length != c.V.length) {
+    throw new Error("XorSigma is of invalid length");
+  }
+  const sigma = xor(hgidt, c.V);
+
+  // 2. Compute M = W XOR H4(sigma)
+  const hsigma = h4(sigma, c.W.length);
+
+  const msg = xor(hsigma, c.W);
+
+  // 	3. Check U = rP
+  const r = h3(sigma, msg);
+  const rP = bls.PointG2.BASE.multiply(r);
+
+  if (!rP.equals(c.U)) {
+    throw new Error("invalid proof: rP check failed");
+  }
+
+  return msg;
+}
+
+export function gtToHash(gt: bls.Fp12, len: number): Uint8Array {
+  return sha256
+    .create()
+    .update("IBE-H2")
+    .update(fp12ToBytes(gt))
+    .digest()
+    .slice(0, len);
+}
+
+// Our IBE hashes
+const BitsToMaskForBLS12381 = 1;
+
+function h3(sigma: Uint8Array, msg: Uint8Array) {
+  const h3ret = sha256
+    .create()
+    .update("IBE-H3")
+    .update(sigma)
+    .update(msg)
+    .digest();
+
+  // We will hash iteratively: H(i || H("IBE-H3" || sigma || msg)) until we get a
+  // value that is suitable as a scalar.
+  for (let i = 1; i < 65535; i++) {
+    let data = h3ret;
+    data = sha256
+      .create()
+      .update(create16BitUintBuffer(i))
+      .update(data)
+      .digest();
+    // assuming Big Endianness
+    data[0] = data[0] >> BitsToMaskForBLS12381;
+    const n = bytesToNumberBE(data);
+    if (n < bls.CURVE.r) {
+      return n;
+    }
+  }
+
+  throw new Error("invalid proof: rP check failed");
+}
+
+function h4(sigma: Uint8Array, len: number): Uint8Array {
+  const h4sigma = sha256.create().update("IBE-H4").update(sigma).digest();
+
+  return h4sigma.slice(0, len);
+}
+
+function create16BitUintBuffer(input: number): Buffer {
+  if (input < 0) {
+    throw Error("cannot write a negative value as uint!");
+  }
+  if (input > 2 ** 16) {
+    throw Error("input value too large to fit in a uint16!");
+  }
+
+  const buf = Buffer.alloc(2);
+  buf.writeUint16LE(input);
+  return buf;
+}
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { PointG1 } from "@noble/bls12-381";
+import * as ibe from "./crypto/ibe";
+import { encryptAge } from "./age/age-encrypt-decrypt";
+import { Buffer } from "buffer";
+
+type Stanza = {
+  type: string;
+  args: Array<string>;
+  body: Uint8Array;
+};
+
+interface Ciphertext {
+  U: PointG1;
+  V: Uint8Array;
+  W: Uint8Array;
+}
+
+function createTimelockEncrypter(id: number, pubKey: string) {
+  return async (fileKey: Uint8Array): Promise<Array<Stanza>> => {
+    const idByte = new TextEncoder().encode(id.toString());
+    const point = PointG1.fromHex(pubKey);
+    const ciphertext = await ibe.encryptOnG1(point, idByte, fileKey);
+    return [
+      {
+        type: "distIBE",
+        args: [`${id}`],
+        body: serialisedCiphertext(ciphertext),
+      },
+    ];
+  };
+}
+
+function serialisedCiphertext(ciphertext: Ciphertext): Uint8Array {
+  return Buffer.concat([
+    ciphertext.U.toRawBytes(true),
+    ciphertext.V,
+    ciphertext.W,
+  ]);
+}
+
+export async function timelockEncrypt(
+  id: number,
+  pubKey: string,
+  payload: Buffer
+): Promise<string> {
+  const timelockEncrypter = createTimelockEncrypter(id, pubKey);
+  const agePayload = await encryptAge(payload, timelockEncrypter);
+  return Buffer.from(agePayload, "binary").toString("hex");
+}
